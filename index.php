aa7a7dd0c14275ab790ff4f5c93722bd6243e112
M.Kichman <32820793+w57015@users.noreply.github.com>
2018-04-10 17:45:38 +0200

    Wstep do logowania

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+   
+    $url = $_GET['page'];
+    $file = "pages/".$url.".php";
+    
+    if(file_exists($file)) {
+        include($file);
+    } else {
+        include("pages/error.php");
+    }
+
+//include("home.php");
+?>
---
+++ portfolio.php
@@ -0,0 +1,75 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml-transitionsl.dtd">
+<html xmins="http://www.w3.org/1999/xhtml" xml:lang"pl" lang"pl">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta http-equiv="content-language" content="pl" />
+	
+	<meta name="author" content="Magdalena Kichman" />
+	<meta name="robots" content="index, follow" />
+	
+	<title> Portfolio | Usługi marketingowe - Karpatia Promotion </title>
+	
+	<meta name="description" content ="Usługi marketingowe w zakresie pozycjonowania stron internetowych w wyszukiwarce Google - SEO" />
+	<meta name="keywords" content="pozycjonowanie, SEO, google, strony internetowe, marketing" />
+		<link rel="stylesheet" href="style.css" type="text/css"/>
+		
+	</head>
+	<body>
+		<!--<div align="center">   NIE DZIALA PRZEZ TO LOGO-->
+			<div id="kontener">
+				<div id="gorna_czesc_kontenera">
+					<a href="index.php"><img class "logo" src="img/logo.gif" alt="Logo CarpatiaPromotion.pl" /></a>
+					<div id="gorne_menu">
+						<ul id="wlasciwe_gorne_menu">
+		
+							<li> <a href="index.php?page=home"><span class"link">Strona główna</span></a></li>
+							<li> <a href="index.php?page=uslugi"><span class"link">Usługi </span></a></li>
+							<li> <a href="index.php?page=portfolio"><span class"link">Portfolio </span></a></li>
+                                                        <li> <a href="index.php?page=e-learning"><span class"link">E-learning </span></a></li>
+							<li> <a href="index.php?page=kontakt"><span class"link">Kontakt </span></a></li>
+						</ul>
+					</div>	
+					<!-- koniec div gorne_menu -->		
+				</div>
+				<!-- koniec div gorna_czesc_kontenera -->
+				<div id="zawartosc">
+				<div id="srodkowa_czesc_zawartosci">
+				
+				
+					<p> Tutaj będą super chwytające fotki portfolio. </p>
+					<p> Ale narazie jest koteł. </p>
+					
+						<img src = "img/kot.png" alt="kot">
+				</div>
+				<!-- koniec div srodkowa_czesc_zawartosci -->
+				</div>
+				<!-- koniec div zawartosc -->
+			
+				<div id="dolna_czesc_zawartosci">
+				
+				
+				</div>
+				<!-- koniec div dolna_czesc_zawartosci -->
+					<div id="copyright">
+						&copy; 2018 created by <strong> Magdalena Kichman </strong> 
+					
+					</div>
+				<!-- koniec div copyright -->
+			</div>
+			<!-- koniec div zawartosc -->
+				
+			</div>
+			 <!--koniec div kontener -->
+	</body>
+	
+	
+	
+	
+</html>
+<?php
+
+
